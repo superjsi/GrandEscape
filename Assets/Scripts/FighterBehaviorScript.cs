6d2411239923a72e897676b2e67e7c6fdee91b36
ray.jeong <ray.s.jeong@selvas.com>
2019-06-13 21:31:30 +0900

    first commit

---
+++ Assets/Scripts/FighterBehaviorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class FighterBehaviorScript : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position), out hit, 10000))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MothershipBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class MothershipBehavior : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private GameObject motherShipGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        motherShipGameObject = GameObject.Find("MotherShip");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var fighter = GameObject.Find("Fighter");
+        var distanceFromFighter = Vector3.Distance(fighter.transform.position, motherShipGameObject.transform.position);
+
+        agent.speed = distanceFromFighter * 0.1f;
+
+        if (distanceFromFighter > 100)
+            agent.speed = 0;
+
+        agent.destination = Vector3.forward;
+
+    }
+}
