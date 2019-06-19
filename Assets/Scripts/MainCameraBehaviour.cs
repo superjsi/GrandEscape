b3c2bc6051931b7e3a43545760df3bed8a64de85
ray.jeong <ray.s.jeong@selvas.com>
2019-06-19 16:20:01 +0900

    ggg

---
+++ Assets/Scripts/MainCameraBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraBehaviour : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("HumanFighter");
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/MothershipBehavior.cs
+++ Assets/Scripts/MothershipBehavior.cs
@@ -1,31 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UIElements;
+
 
 public class MothershipBehavior : MonoBehaviour
 {
     private NavMeshAgent agent;
     private GameObject motherShipGameObject;
+    private GameObject portalGameObject;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        motherShipGameObject = GameObject.Find("MotherShip");
+        motherShipGameObject = GameObject.Find("HumanMotherShip");
+        portalGameObject = GameObject.Find("Portal");
+        agent.destination = portalGameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var fighter = GameObject.Find("Fighter");
-        var distanceFromFighter = Vector3.Distance(fighter.transform.position, motherShipGameObject.transform.position);
+        var fighter = GameObject.Find("HumanFighter");
 
+        var distanceFromFighter = Vector3.Distance(fighter.transform.position, motherShipGameObject.transform.position);
         agent.speed = distanceFromFighter * 0.1f;
 
         if (distanceFromFighter > 100)
             agent.speed = 0;
-
-        agent.destination = Vector3.forward;
-
     }
 }
